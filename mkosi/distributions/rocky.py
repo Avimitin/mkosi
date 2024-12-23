f0344362c328fbf651639a80b054f4793ebda7f6
Avimitin <dev@avimit.in>
2024-12-23 16:11:42 +0800

    rocky: backport rocky 8 support

--- mkosi/distributions/rocky.py
+++ mkosi/distributions/rocky.py
@@ -1,8 +1,14 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
 
+from collections.abc import Iterable
 from mkosi.context import Context
 from mkosi.distributions import centos, join_mirror
 from mkosi.installer.rpm import RpmRepository, find_rpm_gpgkey
+from mkosi.versioncomp import GenericVersion
+from mkosi.log import die
+from mkosi.installer.dnf import Dnf
+from mkosi.installer.rpm import RpmRepository, find_rpm_gpgkey, setup_rpm
+from mkosi.util import listify
 
 
 class Installer(centos.Installer):
@@ -32,3 +38,31 @@ class Installer(centos.Installer):
     @classmethod
     def sig_repositories(cls, context: Context) -> list[RpmRepository]:
         return []
+
+    @classmethod
+    def setup(cls, context: Context) -> None:
+        if GenericVersion(context.config.release) <= 7:
+            die(f"{cls.pretty_name()} 7 or earlier variants are not supported")
+
+        Dnf.setup(context, cls.repositories(context))
+        (context.sandbox_tree / "etc/dnf/vars/stream").write_text(f"{context.config.release}-stream\n")
+        setup_rpm(context, dbpath=cls.dbpath(context))
+
+    @classmethod
+    @listify
+    def repositories(cls, context: Context) -> Iterable[RpmRepository]:
+        if context.config.local_mirror:
+            yield from cls.repository_variants(context, "AppStream")
+            return
+
+        yield from cls.repository_variants(context, "BaseOS")
+        yield from cls.repository_variants(context, "AppStream")
+        yield from cls.repository_variants(context, "extras")
+
+        if GenericVersion(context.config.release) >= 9:
+            yield from cls.repository_variants(context, "CRB")
+        else:
+            yield from cls.repository_variants(context, "PowerTools")
+
+        yield from cls.epel_repositories(context)
+        yield from cls.sig_repositories(context)
